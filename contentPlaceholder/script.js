be906f42b331cd4f6254e70f39425a2980cce7ae
Dakota O'Neil <oneilkody@gmail.com>
2022-02-02 21:34:36 -0600

    i dont know i guess this will be a bad commit message

--- contentPlaceholder/script.js
+++ contentPlaceholder/script.js
@@ -12,7 +12,7 @@ function getData() {
     header.innerHTML = '<img src="https://source.unsplash.com/random/300x300" alt="random photo"/>'
     title.innerHTML =  'Lorem ipsum dolor sit amet.'
     excerpt.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, atque."
-    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="random author photo">'
+    profile_img.innerHTML = '<img src="https://source.unsplash.com/random/" alt="random author photo">'
     name.innerHTML = 'John Doe'
     date.innerHTML =  'Oct. 8, 2020'
 
